21f5889ba28b8f97bc0f2a3745381ab8bd3cf2a6
Bedoy <carlos.bedoy@gmail.com>
2016-10-17 00:14:53 -0500

    Solved problem with iOS Margin Bottom iOS 10
    When you're using a custom tableViewCell you will get the bottom space on iOS, if you add these lines before on ViewWillAppear fixed the bottom margin, please see my issue on:
    
    https://github.com/slackhq/SlackTextViewController/issues/515

--- Source/SLKTextViewController.m
+++ Source/SLKTextViewController.m
@@ -196,6 +196,9 @@ CGFloat const SLKAutoCompletionViewDefaultHeight = 140.0;
         // Reloads any cached text
         [self slk_reloadTextView];
     }];
+    
+    [self setAutomaticallyAdjustsScrollViewInsets:NO];
+    [[self tableView] setContentInset:UIEdgeInsetsZero];
 }
 
 - (void)viewDidAppear:(BOOL)animated
