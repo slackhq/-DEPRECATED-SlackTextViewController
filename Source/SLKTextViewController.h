2b25138146be4d6d59aee2c615b8d57f0d75be18
Elliott Minns <elliott.minns@me.com>
2017-07-13 17:26:40 +0100

    Removed readonly from the image property

--- Source/SLKTextViewController.h
+++ Source/SLKTextViewController.h
@@ -64,7 +64,7 @@ NS_CLASS_AVAILABLE_IOS(7_0) @interface SLKTextViewController : UIViewController 
 /** The default typing indicator used to display user names horizontally. */
 @property (nonatomic, readonly) SLKTypingIndicatorView *_Nullable typingIndicatorView;
 
-@property (nonatomic, readonly, nullable) UIImage *image;
+@property (nonatomic, nullable) UIImage *image;
 
 /**
  The custom typing indicator view. Default is kind of SLKTypingIndicatorView.
